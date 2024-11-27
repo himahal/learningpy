7d484866891787b44fbb180b80df91c495c4717b
himahal <breno.pizolitto@discente.univasf.edu.br>
2024-11-27 15:45:10 -0300

    2

--- a/learning.py
+++ a/learning.py
@@ -1,2 +1,15 @@
-breno = "teste"
-brano = "@"
+nota1 = int(input("Qual a sua primeira nota? "))
+nota2 = int(input("Qual a sua segunda nota?\n"))
+
+media = (nota1 + nota2)/2
+
+if (media >= 7 and media <= 10):
+    print("Parabéns você foi aprovado! Sua média foi", media)
+elif (media <7 and media >= 4):
+    print("Você pegou final!")
+else:
+    print("Você foi reprovado!")
+
+
+
+
