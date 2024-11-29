b4d04ffcf6698e473421f3d08508c426bde2301d
himahal <breno.pizolitto@discente.univasf.edu.br>
2024-11-28 21:15:38 -0300

    3

--- a/learning.py
+++ a/learning.py
@@ -1,4 +1,4 @@
-nota1 = int(input("Qual a sua primeira nota? "))
+'''nota1 = int(input("Qual a sua primeira nota? "))
 nota2 = int(input("Qual a sua segunda nota?\n"))
 
 media = (nota1 + nota2)/2
@@ -7,9 +7,18 @@ if (media >= 7 and media <= 10):
     print("Parabéns você foi aprovado! Sua média foi", media)
 elif (media <7 and media >= 4):
     print("Você pegou final!")
+    n3 = int(input("Digite sua nota da final: "))
+    media1 = (media + n3)/2
+    if (media1 <= 10 and media1 >= 4):
+        print("Parabéns!!! Você foi aprovado por média! Sua média final foi:", media1)
+    else:
+        print("Você foi reprovado!")
+
 else:
     print("Você foi reprovado!")
 
-
-
+print("A media foi {}".format(media))
+'''
+lista = [input("Digite nomes")]
+print(lista)
 
